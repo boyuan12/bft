3859293dd4d91f7aa515d60b39c336d4773f00e4
Boyuan Liu <boyuanliu6@yahoo.com>
2022-01-29 12:06:20 -0800

    change all input to str

--- grade/__init__.py
+++ grade/__init__.py
@@ -27,39 +27,39 @@ def output_A_for_100():
 
 @check50.check()
 def output_A_for_95():
-  check50.run("python3 main.py").stdin(95).stdout("A")
+  check50.run("python3 main.py").stdin("95").stdout("A")
 
 @check50.check()
 def output_A_for_90():
-  check50.run("python3 main.py").stdin(90).stdout("A")
+  check50.run("python3 main.py").stdin("90").stdout("A")
 
 @check50.check()
 def output_B_for_80():
-  check50.run("python3 main.py").stdin(80).stdout("B")
+  check50.run("python3 main.py").stdin("80").stdout("B")
 
 @check50.check()
 def output_C_for_79():
-  check50.run("python3 main.py").stdin(79).stdout("C")
+  check50.run("python3 main.py").stdin("79").stdout("C")
 
 @check50.check()
 def output_invalid_for_input_greater_than_100():
-  check50.run("python3 main.py").stdin(random.randint(101, 500)).stdout("Invalid")
+  check50.run("python3 main.py").stdin(str(random.randint(101, 500))).stdout("Invalid")
 
 @check50.check()
 def output_invalid_for_input_greater_than_100():
-  check50.run("python3 main.py").stdin(random.randint(-100, 1)).stdout("Invalid")
+  check50.run("python3 main.py").stdin(str(random.randint(-100, 1))).stdout("Invalid")
 
 @check50.check()
 def random_check_1():
   num = random.randint(1, 101)
-  check50.run("python3 main.py").stdin(num).stdout(answer(num))
+  check50.run("python3 main.py").stdin(str(num)).stdout(answer(num))
 
 @check50.check()
 def random_check_2():
   num = random.randint(1, 101)
-  check50.run("python3 main.py").stdin(num).stdout(answer(num))
+  check50.run("python3 main.py").stdin(str(num)).stdout(answer(num))
 
 @check50.check()
 def random_check_3():
   num = random.randint(1, 101)
-  check50.run("python3 main.py").stdin(num).stdout(answer(num))
+  check50.run("python3 main.py").stdin(str(num)).stdout(answer(num))

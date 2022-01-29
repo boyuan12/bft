66f0e89a5f33d25d9a47491ac030c64fa61bb574
Boyuan Liu <boyuanliu6@yahoo.com>
2022-01-29 11:59:20 -0800

    fixed indentation for graed

--- grade/__init__.py
+++ grade/__init__.py
@@ -2,9 +2,9 @@ import check50
 import random
 
 def answer(grade):
-	grade = int(grade)
+  grade = int(grade)
   if grade > 100 or grade < 1:
-	  return "Invalid"
+    return "Invalid"
   else:
     if grade >= 90 and grade <= 100:
       return "A"
@@ -20,15 +20,15 @@ def answer(grade):
 @check50.check()
 def exists():
   check50.exists("main.py")
-  
+
 @check50.check()
 def output_A_for_100():
   check50.run("python3 main.py").stdin("100").stdout("A")
- 
+
 @check50.check()
 def output_A_for_95():
   check50.run("python3 main.py").stdin(95).stdout("A")
-  
+
 @check50.check()
 def output_A_for_90():
   check50.run("python3 main.py").stdin(90).stdout("A")
@@ -53,12 +53,12 @@ def output_invalid_for_input_greater_than_100():
 def random_check_1():
   num = random.randint(1, 101)
   check50.run("python3 main.py").stdin(num).stdout(answer(num))
-  
+
 @check50.check()
 def random_check_2():
   num = random.randint(1, 101)
   check50.run("python3 main.py").stdin(num).stdout(answer(num))
-  
+
 @check50.check()
 def random_check_3():
   num = random.randint(1, 101)
